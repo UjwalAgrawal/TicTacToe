20ccc27da8759162c7fdad20819b84060264b1f5
UjwalAgrawal <uvagrawal123@gmail.com>
2020-09-06 12:29:30 +0530

    logical issue fix

--- app/src/main/java/com/ujwal/tictactoe/MainActivity.java
+++ app/src/main/java/com/ujwal/tictactoe/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     //0: cross 1:zero 2: Have not been tapped
     int activePlayer = 0;
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
-    int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8} ,{0,3,6},{1,4,7},{2,5,8}, {0,5,8}, {2,5,6}};
+    int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8} ,{0,3,6},{1,4,7},{2,5,8}, {0,4,8}, {2,4,6}};
     boolean gameActive = true;
 
     public void appear(View view){
